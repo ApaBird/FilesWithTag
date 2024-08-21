4cff79d27ccca8307fb0dda33e584b301375d0b0
Stepan Butorin <Kuramchika@gmail.com>
2024-08-21 10:24:19 +0800

    build

---
+++ frontend/server/chunks/build/welcome-styles.CKA9Gh02.mjs
@@ -0,0 +1,6 @@
+import { w as welcome_vue_vue_type_style_index_0_scoped_8526c2ef_lang } from './welcome-styles-1.mjs-Cl74qY_t.mjs';
+
+const welcomeStyles_CKA9Gh02 = [welcome_vue_vue_type_style_index_0_scoped_8526c2ef_lang, welcome_vue_vue_type_style_index_0_scoped_8526c2ef_lang];
+
+export { welcomeStyles_CKA9Gh02 as default };
+//# sourceMappingURL=welcome-styles.CKA9Gh02.mjs.map
