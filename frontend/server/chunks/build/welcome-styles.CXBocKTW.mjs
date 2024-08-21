1078f16250d8a520cfc81a5ee7ee24fe1834a4c4
Stepan Butorin <Kuramchika@gmail.com>
2024-08-21 11:05:26 +0800

    наконецто

--- frontend/server/chunks/build/welcome-styles.CXBocKTW.mjs
+++
@@ -1,6 +0,0 @@
-import { w as welcome_vue_vue_type_style_index_0_scoped_8526c2ef_lang } from './welcome-styles-1.mjs-Cl74qY_t.mjs';
-
-const welcomeStyles_CXBocKTW = [welcome_vue_vue_type_style_index_0_scoped_8526c2ef_lang];
-
-export { welcomeStyles_CXBocKTW as default };
-//# sourceMappingURL=welcome-styles.CXBocKTW.mjs.map
