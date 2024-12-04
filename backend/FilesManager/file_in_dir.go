4e5f27585b293edb99350164db0e978ac5564a60
= <=>
2024-12-04 11:49:23 +0800

    FastLoad /Files

--- backend/FilesManager/file_in_dir.go
+++ backend/FilesManager/file_in_dir.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"path"
 	"strings"
+	"sync"
+	"time"
 )
 
 type convector struct {
@@ -21,11 +23,13 @@ type Content struct {
 var OsTree = Dir{}
 
 func FilesInDir(dir string, count, offset int, ftype string) ([]Content, error) {
+	satrtT := time.Now()
 	files, err := os.ReadDir(dir)
 	if err != nil {
 		return nil, err
 	}
 	var list []Content
+	wg := sync.WaitGroup{}
 	for _, file := range files {
 		if file.IsDir() {
 			continue
@@ -43,24 +47,49 @@ func FilesInDir(dir string, count, offset int, ftype string) ([]Content, error) 
 		}
 
 		pathToFile := strings.Replace(dir+"/"+file.Name(), "\\", "/", -1)
-		f, b, err := OpenFile(pathToFile)
-		if err != nil {
-			fmt.Println("[ERROR] ", err.Error())
-			continue
-		}
 
-		res := Content{
-			Name:    file.Name(),
-			Ftype:   f.ftype,
-			Tags:    f.GetTags().ToSlice(),
-			Content: b,
-		}
+		wg.Add(1)
+		go asyncFilesInDir(pathToFile, &list, &wg, &sync.Mutex{})
 
-		list = append(list, res)
+		// f, b, err := OpenFile(pathToFile)
+		// if err != nil {
+		// 	fmt.Println("[ERROR] ", err.Error())
+		// 	continue
+		// }
+
+		// res := Content{
+		// 	Name:    file.Name(),
+		// 	Ftype:   f.ftype,
+		// 	Tags:    f.GetTags().ToSlice(),
+		// 	Content: b,
+		// }
+
+		// list = append(list, res)
 		count -= 1
 		if count == 0 {
 			break
 		}
 	}
+	wg.Wait()
+	fmt.Println("[DEBUG] ", time.Since(satrtT).Seconds(), "sec")
 	return list, nil
+}
+
+func asyncFilesInDir(pathFile string, list *[]Content, wg *sync.WaitGroup, m *sync.Mutex) {
+	defer wg.Done()
+	f, b, err := OpenFile(pathFile)
+	if err != nil {
+		fmt.Println("[ERROR] ", err.Error())
+		return
+	}
+	res := Content{
+		Name:    f.Name,
+		Ftype:   f.ftype,
+		Tags:    f.GetTags().ToSlice(),
+		Content: b,
+	}
+
+	m.Lock()
+	*list = append(*list, res)
+	m.Unlock()
 }
